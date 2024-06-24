4e34e85c78dbbb03f1e00d61f7b7d1617b89d03b
Marko Milunovic <markomilunovic9@gmail.com>
2024-06-24 19:59:16 +0200

    Remove serverless adaptation from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { createServer, proxy } from 'aws-serverless-express';
-
-const expressApp = express();
-let server: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  app.enableCors();
-  await app.init();
-  server = createServer(expressApp);
-};
-
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
 bootstrap();
-
-export const handler = (event: any, context: any) => {
-  return proxy(server, event, context);
-};
